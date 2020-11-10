4ddc7497cb4dd896c640ac79c4e654ac0c7a5508
Valentin Gérard <valentin.gerard17@hotmail.fr>
2020-11-11 00:12:14 +0100

    Add extension classes
    - Add CORS extension
    - Add IIS integration

---
+++ src/WebAPI/Extensions/IISExtension.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebAPI.Extensions
+{
+    public static class IISExtension
+    {
+        #region Methods
+
+        /// <summary>
+        /// Configure IIS (Internet Information Server) integration
+        /// </summary>
+        /// <param name="services">>Collection to register services</param>
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options => { });
+
+        /// <summary>
+        /// Add IIS (Internet Information Server) integration to application's request pipeline
+        /// </summary>
+        /// <param name="app">Interface for application builder</param>
+        public static void UseIISIntegration(this IApplicationBuilder app) =>
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.All
+            });
+
+        #endregion
+    }
+}
