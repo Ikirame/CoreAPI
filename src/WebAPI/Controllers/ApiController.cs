f5d0e7ecfd93105da2b1b59883e18932f460a3cc
Valentin Gérard <valentin.gerard17@hotmail.fr>
2020-11-11 00:11:36 +0100

    Add ApiController base class

---
+++ src/WebAPI/Controllers/ApiController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public abstract class ApiController : ControllerBase
+    {
+        #region Fields
+
+        protected readonly ILogger Logger;
+
+        #endregion
+
+        #region Constructors
+
+        protected ApiController(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        #endregion
+    }
+}
