539642c9786d25db46cfb17b1166bf2540d35e14
Valentin Gerard <valentin.gerard17@hotmail.fr>
2025-01-23 00:40:02 +0100

    Add Storage bundle with database provider interface and an implementation for mongo db

---
+++ bundles/src/CoreAPI.Storage.Bundle/Database/IDatabaseProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace CoreAPI.Storage.Bundle.Database;
+
+public interface IDatabaseProvider
+{
+    Task<T> CreateAsync<T>(T entity, CancellationToken ctx) where T : class;
+    Task<T> GetOneAsync<T>(Guid id, CancellationToken ctx) where T : class;
+    Task<T> ReplaceAsync<T>(Guid id, T entity, CancellationToken ctx) where T : class;
+    Task DeleteAsync<T>(Guid id, CancellationToken ctx) where T : class;
+}
---
+++ bundles/src/CoreAPI.Storage.Bundle/Database/MongoProvider.cs
@@ -0,0 +1,45 @@
+﻿using MongoDB.Driver;
+
+namespace CoreAPI.Storage.Bundle.Database;
+
+public sealed class MongoProvider : IDatabaseProvider
+{
+    private readonly IMongoDatabase _mongoDatabase;
+    
+    private IMongoCollection<T> GetCollection<T>() => _mongoDatabase.GetCollection<T>(nameof(T));
+
+    public MongoProvider(string connectionString, string databaseName)
+    {
+        MongoClient mongoClient = new(connectionString);
+        _mongoDatabase = mongoClient.GetDatabase(databaseName);
+    }
+
+    public async Task<T> CreateAsync<T>(T entity, CancellationToken ctx) where T : class
+    {
+        InsertOneOptions options = new();
+        await GetCollection<T>().InsertOneAsync(entity, options, ctx);
+        
+        return entity;
+    }
+
+    public async Task<T> GetOneAsync<T>(Guid id, CancellationToken ctx) where T : class
+    {
+        var filter = Builders<T>.Filter.Eq("id", id);
+        return await GetCollection<T>().Find(filter).FirstOrDefaultAsync(ctx);
+    }
+
+    public async Task<T> ReplaceAsync<T>(Guid id, T entity, CancellationToken ctx) where T : class
+    {
+        var filter = Builders<T>.Filter.Eq("id", id);
+        ReplaceOptions replaceOptions = new();
+        
+        await GetCollection<T>().ReplaceOneAsync(filter, entity, replaceOptions, ctx);
+        return entity;
+    }
+
+    public async Task DeleteAsync<T>(Guid id, CancellationToken ctx) where T : class
+    {
+        var filter = Builders<T>.Filter.Eq("id", id);
+        await GetCollection<T>().DeleteOneAsync(filter, ctx);
+    }
+}
